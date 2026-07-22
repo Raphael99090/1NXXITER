44a7cc2a268167fe2b404986b7f724917b322e3d
Raphael Viana Sardinha <raphaelpok945@gmail.com>
2026-07-22 13:52:22 -0300

    Implement MovementTab with player mod options

--- src/Interface/Tabs/MovementTab.lua
+++ src/Interface/Tabs/MovementTab.lua
@@ -1,1 +1,15 @@
+local Tab = {}
 
+function Tab:Render(WindowTab, Hub, Config)
+    local Mod = Hub.Features.PlayerMods
+    
+    WindowTab:AddSection("Atributos")
+    WindowTab:AddToggle("SpeedE", { Title = "Ativar Speed", Default = false, Callback = function(v) Mod:ToggleSpeed(v) end })
+    WindowTab:AddSlider("SpeedV", { Title = "Velocidade", Min = 16, Max = 500, Default = 50, Rounding = 0, Callback = function(v) Mod.Settings.SpeedValue = v end })
+    
+    WindowTab:AddSection("Física")
+    WindowTab:AddToggle("NoclipE", { Title = "Atravessar Paredes", Default = false, Callback = function(v) Mod:ToggleNoclip(v) end })
+    WindowTab:AddToggle("InfJumpE", { Title = "Pulo Infinito", Default = false, Callback = function(v) Mod:ToggleInfJump(v) end })
+end
+
+return Tab
